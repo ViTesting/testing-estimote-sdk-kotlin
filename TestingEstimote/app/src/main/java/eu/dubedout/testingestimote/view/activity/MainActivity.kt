aba83d51a13a120c8062538492c8d3484cf85968
Vincent Dubedout <vincent.dubedout@gmail.com>
2016-02-07 17:39:40 -0500

    added first app files

---
+++ TestingEstimote/app/src/main/java/eu/dubedout/testingestimote/view/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package eu.dubedout.testingestimote.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import eu.dubedout.testingestimote.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
